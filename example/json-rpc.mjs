ad97959d306e6b914426421f165904c1947b7e2f
aslanbayramov <117563304+aslanbayramov@users.noreply.github.com>
2023-01-23 03:35:33 +0400

    Update json-rpc.mjs

--- example/json-rpc.mjs
+++ example/json-rpc.mjs
@@ -1,22 +1,17 @@
-require('dotenv').config();
-
-const { Api, JsonRpc } = require('ineryjs');
-const { JsSignatureProvider } = require('ineryjs/dist/ineryjs-jssig');
-const { PrivateKey } = require('ineryjs/dist/PrivateKey');
-
-const url = process.env.URL;
-const private_key = process.env.PRIVATE_KEY;
-const actor = process.env.ACTOR;
-const account = process.env.ACCOUNT;
-
-const signatureProvider = new JsSignatureProvider([private_key]);
-const rpc = new JsonRpc(url);
-const api = new Api({ rpc, signatureProvider });
-
-const tx = api.buildTransaction();
-tx.with(account).as(actor).create("id", "user", { data: "example" });
-tx.send().then((result) => {
-    console.log(result);
-}).catch((error) => {
+import dotenv from 'dotenv';
+dotenv.config();
+import {Api} from 'ineryjs';
+const api = new Api({url:process.env.url});
+const account = process.env.account;
+const privateKey = process.env.private_key;
+const actor = process.env.actor;
+const data = {...};
+              
+try {
+    const tx = api.buildTransaction();
+    tx.with(account).as(actor).action_name(data);
+    tx.sign(privateKey);
+    console.log(await tx.send());
+} catch(error) {
     console.error(error);
-});
+}
