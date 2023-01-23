2e6096d7b61f87eaa8346119a9f2cf2d22bac193
aslanbayramov <117563304+aslanbayramov@users.noreply.github.com>
2023-01-23 04:44:19 +0400

    Update json-rpc.mjs

--- example/json-rpc.mjs
+++ example/json-rpc.mjs
@@ -1,17 +1,24 @@
-import dotenv from 'dotenv';
-dotenv.config();
-import {Api} from 'ineryjs';
-const api = new Api({url:process.env.url});
-const account = process.env.account;
-const privateKey = process.env.private_key;
-const actor = process.env.actor;
-const data = {...};
-              
-try {
+import { Api, JsonRpc, RpcError, JsSignatureProvider } from 'ineryjs/dist/index.js'
+
+const url = process.env.NODE_URL
+
+const json_rpc = new JsonRpc(url)
+const private_key = process.env.PRIVATE_KEY;
+
+const account = process.env.INERY_ACCOUNT
+const actor = process.env.INERY_ACCOUNT
+const signature  = new JsSignatureProvider([private_key]);
+
+const api = new Api({
+  rpc: json_rpc,
+  signatureProvider: signature
+})
+
+async function create(id, user, data){
+  try{
     const tx = api.buildTransaction();
-    tx.with(account).as(actor).action_name(data);
-    tx.sign(privateKey);
+    tx.with(account).as(actor).create({id, user, data});
     console.log(await tx.send());
-} catch(error) {
-    console.error(error);
-}
+} catch (error) {
+  console.log(error);
+  }
