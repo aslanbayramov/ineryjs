c6191653eba85f3b1384fedeb89b402070c08937
aslanbayramov <117563304+aslanbayramov@users.noreply.github.com>
2023-01-23 23:13:50 +0400

    Update rpc.js

--- example/rpc.js
+++ example/rpc.js
@@ -2,15 +2,15 @@ const Inery = require("ineryjs");
 
 // Connect to Inery node
 
-const inery = new Inery("http://localhost:4000");
+const inery = new Inery("http://5.75.228.144:8888");
 
 // Build the transaction
 
-const tx = inery.transaction.transfer("INR", "Address_of_the_recipient", "100", "memo");
+const tx = inery.transaction.transfer("INR", "imery", "0.0001", "memo");
 
 // Sign the transaction
 
-const signedTx = inery.transaction.sign(tx, "private_key_of_the_sender");
+const signedTx = inery.transaction.sign(tx, "5KNyNrcgF5BoaDZcBd9BTAFTXC3RPsBud358aA8nsC7ZXDwJq8o");
 
 // Send the transaction
 
